769207a63f36389ef80bf5f7cc7024a9ba299321
lgope <ilakshmangope@gmail.com>
2020-02-11 00:14:33 +0600

    lambda expression python..

--- basics/lambda.py
+++ basics/lambda.py
@@ -1,14 +1,22 @@
 # add
-
-
-def add(a): return a + 10
-
+add = lambda a : a + 10
 
 print(add(5))
 
 
 # multiply
-def multiply(a, b): return a * b
-
+multiply = lambda a, b : a * b
 
 print(multiply(5, 6))
+
+
+# Lambda Functions
+def myfunc(n):
+    return lambda a: a * n
+
+
+mydoubler = myfunc(2)
+anotherdoubler = myfunc(4)
+
+print(mydoubler(11))
+print(anotherdoubler(4))
