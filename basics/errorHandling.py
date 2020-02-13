36e953a9f6f6737fa8b5321a5ff988a81ff12968
lgope <ilakshmangope@gmail.com>
2020-02-14 01:05:37 +0600

    error handling py 🛠

---
+++ basics/errorHandling.py
@@ -0,0 +1,19 @@
+def __devideFunc (num1, num2):
+    try:
+        result = int(num1) / int(num2)
+    except ZeroDivisionError:
+        print('division by zero!')
+    except ValueError:
+        print('Please enter an integer')
+    else:
+        print(f'result is {result}')
+    finally:
+        print('finally clause')
+
+__devideFunc(2, 1)
+
+
+__devideFunc(2, 0)
+
+
+__devideFunc("2", "kjkjk")
