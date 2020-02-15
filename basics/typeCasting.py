7ff94028b5dd63572f8999e60c86df047a075577
lgope <ilakshmangope@gmail.com>
2020-02-15 23:30:14 +0600

    Python type Casting 🔗

---
+++ basics/typeCasting.py
@@ -0,0 +1,22 @@
+# int()
+x = int(1) # x will be 1
+y = int(4.8) # y will be 4
+z = int("6") # z will be 6
+
+print(x, y, z)
+
+# float()
+x = float(4)     # x will be 4.0
+y = float(4.8)   # y will be 4.8
+z = float("8")   # z will be 8.0
+w = float("9.2") # w will be 9.2
+
+print(x, y, z, w)
+
+
+# str()
+x = str("s4") # x will be 's4'
+y = str(4)    # y will be '4'
+z = str(9.0)  # z will be '9.0'
+
+print(x, y, z)
