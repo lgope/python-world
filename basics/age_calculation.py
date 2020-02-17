d2d944e0cd62837ba4c9e9b67c4231907e45e6a5
lgope <ilakshmangope@gmail.com>
2020-02-17 21:50:43 +0600

    type conversion basics syntex..

---
+++ basics/age_calculation.py
@@ -0,0 +1,3 @@
+birth_year = input("What year were you born? ")
+age = 2020 - int(birth_year)
+print(f'Your age is : {age}')
--- basics/typeCasting.py
+++ basics/typeCasting.py
@@ -1,22 +1,26 @@
 # int()
-x = int(1) # x will be 1
-y = int(4.8) # y will be 4
-z = int("6") # z will be 6
+x = int(1)  # x will be 1
+y = int(4.8)  # y will be 4
+z = int("6")  # z will be 6
 
+a = str(100)
+b = int(a)
+c = type(b)
+
+print(c)
 print(x, y, z)
 
 # float()
-x = float(4)     # x will be 4.0
-y = float(4.8)   # y will be 4.8
-z = float("8")   # z will be 8.0
-w = float("9.2") # w will be 9.2
+x = float(4)  # x will be 4.0
+y = float(4.8)  # y will be 4.8
+z = float("8")  # z will be 8.0
+w = float("9.2")  # w will be 9.2
 
 print(x, y, z, w)
 
-
 # str()
-x = str("s4") # x will be 's4'
-y = str(4)    # y will be '4'
+x = str("s4")  # x will be 's4'
+y = str(4)  # y will be '4'
 z = str(9.0)  # z will be '9.0'
 
 print(x, y, z)
