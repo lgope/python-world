af5e227a01bde77f5125ab2315f071f7f4d11fbf
lgope <ilakshmangope@gmail.com>
2020-02-18 23:07:09 +0600

    py string problem solutions..

---
+++ py-string-problems-solution/name_cases.py
@@ -0,0 +1,5 @@
+person_name = input("Enter your name: ")
+
+print(person_name.lower())
+print(person_name.upper())
+print(person_name.title())
---
+++ py-string-problems-solution/personal_message.py
@@ -0,0 +1,2 @@
+person_name = input("Enter your name: ")
+print(f"Hello {person_name},	would you like to learn some Python today?")
