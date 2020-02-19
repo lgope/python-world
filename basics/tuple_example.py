ee8c4286c6b1f2ac1672e18e808627e1dc94ead5
lgope <ilakshmangope@gmail.com>
2020-02-19 20:34:58 +0600

    tuple example..

---
+++ basics/tuple_example.py
@@ -0,0 +1,8 @@
+thistuple = ("apple", "banana", "cherry", "mango")
+print(thistuple)
+print(type(thistuple))
+
+print(thistuple[1])
+print(thistuple[len(thistuple)-1])
+print(thistuple[-1])
+print(thistuple[2:5])
