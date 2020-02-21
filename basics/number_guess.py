1ce1a7d238f325c26fc0d30bdd105d8d5ef940e7
lgope <ilakshmangope@gmail.com>
2020-02-21 22:00:04 +0600

    number guessing game using random .. 🔢

---
+++ basics/number_guess.py
@@ -0,0 +1,18 @@
+import random
+
+
+def start_game():
+    while True:
+        correct_answer = random.randint(1, 10)
+
+        guess_number = int(input("Guess a number between 1 to 20: "))
+
+        if correct_answer == guess_number:
+            print(f'You win! 👏🎉')
+        else:
+            play_again = input('Try again. 1/0: ')
+            if int(play_again) == 0:
+                break
+
+
+start_game()
