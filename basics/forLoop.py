743c9b6ce91bab97466b53a2662fa56fb3bd4c73
lgope <ilakshmangope@gmail.com>
2020-02-22 16:37:00 +0600

    py loop examples.. 🔃

--- basics/forLoop.py
+++ basics/forLoop.py
@@ -5,8 +5,29 @@ for number in numbers:
     print(product)
 print('The total product is:', product)
 
-
 for x in range(1, 11):
-    print(repr(x).rjust(2), repr(x*x).rjust(3), end=' ')
+    print(repr(x).rjust(2), repr(x * x).rjust(3), end=' ')
     # Note use of 'end' on previous line
-    print(repr(x*x*x).rjust(4))
+    print(repr(x * x * x).rjust(4))
+
+# Use range() to loop through a list of numbers
+for i in range(10):
+    print(i)
+print()
+
+for i in range(10, 20):
+    print(i)
+print()
+
+for i in range(10, 20, 2):
+    print(i)
+print()
+
+primes = [1, 2, 3, 5, 7, 11, 13]
+# Can loop over lists
+for number in primes:
+    print(number)
+
+# If you need to know the index, use enumerate()
+for i, number in enumerate(primes):
+    print(f'{i}: {number}')
---
+++ basics/while_loop.py
@@ -0,0 +1,14 @@
+x = 4
+
+while x >= 0:  # Will keep looping until condition is met
+    print(x)
+    x = x - 1
+
+print()
+
+y = 1
+while True:  # Will keep looping until it encounters a break
+    print(y)
+    y += 1
+    if y == 10:
+        break
